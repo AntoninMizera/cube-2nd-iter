8d075a6e25caa724b519edfbc720f96b375ba893
AntoninMizera <91247749+AntoninMizera@users.noreply.github.com>
2021-11-04 11:42:10 +0100

    allow using from 0 up to 32 sides

--- main.ts
+++ main.ts
@@ -1,90 +1,33 @@
 let canShake = false, sides = 6;
 
-input.onButtonPressed(Button.A, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     canShake = true;
     basic.showIcon(IconNames.Yes);
 });
 
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (sides === 6) {
-        sides = 10;
-        basic.showString("X");
-    } else {
-        sides = 6;
-        basic.showString("6");
-    }
+input.onButtonPressed(Button.A, function () {
+    if (sides > 2) sides--;
+
+    basic.showNumber(sides);
+});
+
+input.onButtonPressed(Button.B, function () {
+    if (sides < 32) sides++;
+
+    basic.showNumber(sides);
 });
 
 input.onGesture(Gesture.Shake, function () {
     if (canShake) {
+        canShake = false;
         let rnd = randint(1, sides);
-
-        if (rnd === 1)
-            basic.showLeds(`. . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .`);
-        else if (rnd === 2)
-            basic.showLeds(`. . . . #
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . .`);
-        else if (rnd === 3)
-            basic.showLeds(`. . . . #
-                . . . . .
-                . . # . .
-                . . . . .
-                # . . . .`);
-        else if (rnd === 4)
-            basic.showLeds(`# . . . #
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . #`);
-        else if (rnd === 5)
-            basic.showLeds(`# . . . #
-                . . . . .
-                . . # . .
-                . . . . .
-                # . . . #`);
-        else if (rnd === 6)
-            basic.showLeds(`# . . . #
-                . . . . .
-                # . . . #
-                . . . . .
-                # . . . #`);
-        else if (rnd === 7)
-            basic.showLeds(`# . . . #
-                . . . . .
-                # . # . #
-                . . . . .
-                # . . . #`);
-        else if (rnd === 8)
-            basic.showLeds(`# . # . #
-                . . . . .
-                # . . . #
-                . . . . .
-                # . # . #`);
-        else if (rnd === 9)
-            basic.showLeds(`# . # . #
-                . . . . .
-                # . # . #
-                . . . . .
-                # . # . #`);
-        else if (rnd === 10)
-            basic.showLeds(`# . . . #
-                . # . # .
-                # . . . #
-                . # . # .
-                # . . . #`);
+        
+        basic.showNumber(rnd);
 
         while (rnd--) {
             music.playTone(Note.C5, music.beat());
             basic.pause(50);
         }
 
-        canShake = false;
     }
 });
